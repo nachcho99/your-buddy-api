08ac0df61450f55eeaec0c74a71ff5924fff848a
ignacioM <ignaciom@ncodingbol.com>
2025-07-26 21:15:08 -0400

    Fix validation type

--- app/Http/Request/ListConversationRequest.php
+++ app/Http/Request/ListConversationRequest.php
@@ -14,16 +14,16 @@ class ListConversationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'page' => ['number'],
-            'size' => ['number', 'min:1', 'max:100'],
+            'page' => ['integer'],
+            'size' => ['integer', 'min:1', 'max:100'],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'page.number' => 'The page must be a number',
-            'size.number' => 'The size must be a number',
+            'page.integer' => 'The page must be a number',
+            'size.integer' => 'The size must be a number',
             'size.min' => 'The size must be at least 1',
             'size.max' => 'The size must not exceed 100',
         ];
